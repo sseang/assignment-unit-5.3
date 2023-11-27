b9ffd5bd6105b27c245b339ae0d71c009f7845ff
sseang <142060734+sseang@users.noreply.github.com>
2023-11-27 17:18:30 -0600

    Commented out albums anb dcalled function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,46 +12,49 @@ const myAlbums = {
   yearPublished: 1971,
 };
 
-const myAlbums2 = {
-  title: 'Master of Reality 2',
-  artist: 'Black Sabbath 2',
-  yearPublished: 1972,
-};
+  
+// const myAlbums2 = {
+//   title: 'Master of Reality 2',
+//   artist: 'Black Sabbath 2',
+//   yearPublished: 1972,
+// };
 
-const myAlbums3 = {
-  title: 'Master of Reality 3',
-  artist: 'Black Sabbath 3',
-  yearPublished: 1973,
-};
+// const myAlbums3 = {
+//   title: 'Master of Reality 3',
+//   artist: 'Black Sabbath 3',
+//   yearPublished: 1973,
+// };
 
-const myAlbums4 = {
-  title: 'Master of Reality 4',
-  artist: 'Black Sabbath 4',
-  yearPublished: 1974,
-};
-const myAlbums5 = {
-  title: 'Master of Reality 5',
-  artist: 'Black Sabbath 5',
-  yearPublished: 1975,
-};
-const myAlbums6 = {
-  title: 'Master of Reality 6',
-  artist: 'Black Sabbath 6',
-  yearPublished: 1976,
-};
+// const myAlbums4 = {
+//   title: 'Master of Reality 4',
+//   artist: 'Black Sabbath 4',
+//   yearPublished: 1974,
+// };
+// const myAlbums5 = {
+//   title: 'Master of Reality 5',
+//   artist: 'Black Sabbath 5',
+//   yearPublished: 1975,
+// };
+// const myAlbums6 = {
+//   title: 'Master of Reality 6',
+//   artist: 'Black Sabbath 6',
+//   yearPublished: 1976,
+// };
+
+
 
 //[x]- Create a function named `addToCollection`. It should have this basic structure:
 function addToCollection(collectionArray, title, artist, yearPublished) {
   let collectionObj = {
     title: title,
     artist: artist,
-    yearPublished: yearPublished
+    yearPublished: yearPublished,
   };
 
   collectionArray.push(collectionObj);
   return collectionObj;
 }
-// console.log('Album 1', addToCollection(myCollection, myAlbums.title, myAlbums.artist, myAlbums.yearPublished));
+console.log('Album 1', addToCollection(myCollection, myAlbums.title, myAlbums.artist, myAlbums.yearPublished));
 // console.log('Album 2', addToCollection(myCollection, myAlbums2.title, myAlbums2.artist, myAlbums2.yearPublished));
 // console.log('Album 3', addToCollection(myCollection, myAlbums3.title, myAlbums3.artist, myAlbums3.yearPublished));
 // console.log('Album 4', addToCollection(myCollection, myAlbums4.title, myAlbums4.artist, myAlbums4.yearPublished));
